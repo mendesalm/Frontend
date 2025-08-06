f30b7d0716c7633e497b206779f585cd95d11d41
mendesalm <mendesalm@gmail.com>
2025-08-06 10:45:39 -0300

    V0047.4

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // Carrega variáveis de ambiente com base no modo (development, production, etc.)
-  const env = loadEnv(mode, ".", ""); // Usa '.' para o diretório atual
+  const env = loadEnv(mode, process.cwd(), "");
 
   return {
     plugins: [react({ jsxRuntime: "automatic" })],
